4b8cbf52fa566d95884a6cc9156532cb6b3e10b9
Djbone75 <Djbone94@hotmail.com>
2023-07-01 20:51:59 +0200

    version 1.1

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const header = document.querySelector(".header");
 const don = document.querySelector(".don");
 const logo = document.querySelector(".logo");
 const news = document.querySelector(".news");
+const presentation = document.querySelector(".presentation");
+const image_presentation = document.querySelector(".image-presentation");
+const headerMobile = document.querySelector(".header__mobile");
 news.style.transform = "translateX(calc(-11rem))";
 
+console.log(presentation);
 const buttonPlus = document.querySelector(".plus");
 const buttonLess = document.querySelector(".less");
+const buttonMobile = document.querySelector("#buttonMobile");
+
+buttonMobile.addEventListener("click", () => {
+  headerMobile.classList.add("active");
+});
+headerMobile.addEventListener("click", () => {
+  headerMobile.classList.remove("active");
+});
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -35,23 +47,38 @@ window.onscroll = function (e) {
   }
 };
 let counter = 2;
-let itemPrevious = document.querySelector(".item--1");
-let slide = () => {
-  let items = document.querySelectorAll(".item");
+let itemPrevious = document.querySelector(".article--1");
 
-  let item = document.querySelector(".item--" + counter);
+let slide = () => {
+  let items = document.querySelectorAll(".article");
+  let item = document.querySelector(".article--" + counter);
 
   items.forEach((item) => {
-    item.classList.remove("item-active");
-    item.classList.remove("item-previous");
+    item.classList.remove("article-active");
+    item.classList.remove("article-previous");
   });
-  item.classList.add("item-active");
-  itemPrevious.classList.add("item-previous");
+  item.classList.add("article-active");
+  itemPrevious.classList.add("article-previous");
   itemPrevious = item;
   counter++;
   counter > 3 ? (counter = 1) : counter;
 };
-let Interval2s = setInterval(slide, 3000);
+let imageNumber = 1;
+presentation.style.backgroundImage =
+  "url('/public/title--" + imageNumber + ".jpg')";
+let hello = () => {
+  imageNumber++;
+  if (imageNumber > 3) {
+    imageNumber = 1;
+  }
+
+  presentation.style.backgroundImage =
+    "url('/public/title--" + imageNumber + ".jpg')";
+  image_presentation.style.backgroundImage =
+    "url('/public/title--" + imageNumber + ".jpg')";
+};
+let Interval3s = setInterval(slide, 3000);
+let newInterval = setInterval(hello, 3000);
 
 let transformX = -11;
 buttonPlus.addEventListener("click", () => {
